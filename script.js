40b22bd4f7f805e4e92b078447c413a3d3bcdbef
danielsilvestre2 <dlds55224@gmail.com>
2026-03-27 09:39:03 -0300

    add more commands

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-let age = 12
-if (age < 17) console.log("Is a Minor")
-else console.log("is older")
+/* const functions1 = (hello) => {
+  console.log('Hello, World!')
+  hello()
+  setTimeout(() => {
+    console.log('Hello Universe!')
+  }, 500)
+}
+
+const hello = () => {
+  setTimeout(() => {
+    console.log('Hello Galaxy!')
+  }, 1000)
+}
+
+functions1(hello) */
+
+// =======================≠=======================
+
+/* const buscarUsuario = (callback) => {
+  const erro = false
+  
+  if (erro) callback('User finded', {nome: "Daniel", idade: 16})
+  else callback('User finded', {nome: "Daniel", idade: 12})
+}
+buscarUsuario((erro, usuario) => {
+  */
+  
+const img = document.querySelector('.viewer')
+  
+const testFunc = async () => {
+  const options = {
+    method: 'get',
+    headers: { 
+      'Content-type': 'application/json',
+      'Authentication': 'Client-Id Q2tld8ltGnBRav2hRUdJihze4IOr05VYzf2vEHjAMy8'
+    }
+  }
+  try {
+    const resp = await fetch('https://jsonplaceholder.com/users')
+    const json = resp.json()
+    console.log(json)
+  }
+  catch(error) {
+    throw new Error('Seever Error')
+  }
+}
+
+testFunc()
